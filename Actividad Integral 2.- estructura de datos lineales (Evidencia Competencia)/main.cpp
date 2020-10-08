e3f694e5b9b9df0916675a21b129a107a1e1580c
JesusUgaldeResendiz <64572948+JesusUgaldeResendiz@users.noreply.github.com>
2020-10-08 09:35:36 -0500

    Update main.cpp

--- Actividad Integral 2.- estructura de datos lineales (Evidencia Competencia)/main.cpp
+++ Actividad Integral 2.- estructura de datos lineales (Evidencia Competencia)/main.cpp
@@ -1,11 +1,19 @@
+/*
+ * Archivo: lib.h
+ *  Creado el: 07/10/2020
+ *  Autor: Jesùs Ugalde Reséndiz
+ *  Matería: TC1031
+ */
 #include <iostream>
 #include "principales.h"
 using namespace std;
 
 int main(){
-
+  
+  /*Entrega de actividad integradora 1*/
   principal1();
-
+  
+  /*Entrega de actividad integradora 2*/
   principal2();
 
   return 0;
