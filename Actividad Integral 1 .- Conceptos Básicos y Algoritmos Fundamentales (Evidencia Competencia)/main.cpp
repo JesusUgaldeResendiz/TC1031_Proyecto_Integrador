9a085bca7eb625cc9966e3582c7875f7f733a504
JesusUgaldeResendiz <64572948+JesusUgaldeResendiz@users.noreply.github.com>
2020-11-26 15:35:22 -0600

    Create main.cpp

---
+++ Actividad Integral 1 .- Conceptos Básicos y Algoritmos Fundamentales (Evidencia Competencia)/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Archivo: main.cpp
+ * Creado: 19/11/2020
+ * Autor: Jesús Ugalde Reséndiz
+ * Descripción: Archivo fuente principal del proyecto.
+*/
+
+#include <iostream>
+#include "aplicacion.h"
+
+using namespace std;
+
+int main(){
+
+  /*Esta función sera la principal en esta actividad su implementación esta en aplicación.h*/
+  principal();
+
+  return 0;
+}
