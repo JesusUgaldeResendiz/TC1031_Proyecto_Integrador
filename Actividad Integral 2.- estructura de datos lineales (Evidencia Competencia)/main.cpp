2bf88e58a28fa3ee4b34d3df5f5592005703c6a2
JesusUgaldeResendiz <64572948+JesusUgaldeResendiz@users.noreply.github.com>
2020-11-27 21:55:31 -0600

    Create main.cpp

---
+++ Actividad Integral 2.- estructura de datos lineales (Evidencia Competencia)/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Archivo: main.cpp
+ * Creado: 19/11/2020
+ * Autor: Jesús Ugalde Reséndiz
+ * Descripción: Archivo fuente para poder correr nuestra aplicación.
+*/
+#include <iostream>
+#include "aplicacion2.h"
+
+int main(){
+
+	/*Se manda llamar la funci�n principal de aplicacion2.h la cual contiene la aplicaci�n.*/
+	principal2();
+}
