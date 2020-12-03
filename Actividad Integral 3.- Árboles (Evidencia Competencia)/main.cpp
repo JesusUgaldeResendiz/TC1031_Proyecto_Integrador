1006000f0470196391965f52fd9d32ca7f5078e5
JesusUgaldeResendiz <64572948+JesusUgaldeResendiz@users.noreply.github.com>
2020-12-03 03:47:53 -0600

    Create main.cpp

---
+++ Actividad Integral 3.- Árboles (Evidencia Competencia)/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Archivo: main.cpp
+ * Creado: 24/11/2020
+ * Autor: Jesús Ugalde Reséndiz
+ * Descripción: Archivo fuente para poder correr nuestra aplicación.
+*/
+
+#include <iostream>
+#include "lecturas.h"
+
+int main(int argc, char *argv[]){
+	/*Llamado de la función principal, o mejor dicho de la aplicación para posteriormente ejecutar en este archivo fuente.*/
+	std::cout<<"===================================="<<std::endl;
+  std::cout<<"  ¡BIENVENIDO A LA BASE DE DATOS!\n"<<std::endl;
+	principal3();
+	std::cout<<"===================================="<<std::endl;
+
+	return 0;
+}
