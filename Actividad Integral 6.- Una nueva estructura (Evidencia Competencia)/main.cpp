4ee9a447213f5f801495be2e7c2121bc37579a37
JesusUgaldeResendiz <64572948+JesusUgaldeResendiz@users.noreply.github.com>
2020-12-04 21:48:27 -0600

    Create main.cpp

---
+++ Actividad Integral 6.- Una nueva estructura (Evidencia Competencia)/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Archivo: main.cpp
+ * Creado: 04/12/2020
+ * Autor: Jesús Ugalde Reséndiz
+ * Descripción: Archivo fuente para poder correr nuestra aplicación.
+*/
+#include <iostream>
+#include "radix.h"
+#include "appextra.h"
+
+int main(int argc, char *argv[]){
+
+  /*Función principal que corre nuestra pequeña aplicación*/
+  extra();
+
+  return 0;
+}
